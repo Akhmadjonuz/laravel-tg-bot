dcd325facb700acd8e386f16e4344b2b20f9d5a0
Ahmadjon Dadabayev <98649872+Akhmadjonuz@users.noreply.github.com>
2023-07-02 23:48:57 +0500

    first commit

---
+++ app/Http/Controllers/TelegramController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Services\TelegramService;
+
+class TelegramController extends Controller
+{
+    public function handle(Request $request)
+    {
+        $input = $request->all();
+        $message = $input['message'];
+        $fid = $message['chat']['id'];
+        $text = $message['text'];
+
+        $bot = new TelegramService;
+
+        if ($text == '/start') {
+            $bot->sendMessage([
+                'chat_id' => $fid,
+                'text' => 'ishladi'
+            ]);
+        }
+    }
+}
